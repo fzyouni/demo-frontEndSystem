0882a228dc292e30d6a6c779b66828791559ebfb
fz <fzyouni@163.com>
2018-07-17 10:30:45 +0800

    登陆页数据修改

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,16 @@
 <template>
-  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px"
+  <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-position="left" label-width="0px"
            class="demo-ruleForm login-container">
     <h3 class="title">系统登录</h3>
     <el-form-item prop="account">
-      <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
+      <el-input type="text" v-model="ruleForm.account" auto-complete="off" placeholder="账号"></el-input>
     </el-form-item>
     <el-form-item prop="checkPass">
-      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
+      <el-input type="password" v-model="ruleForm.checkPass" auto-complete="off" placeholder="密码"></el-input>
     </el-form-item>
     <el-checkbox v-model="checked"  class="remember">记住密码</el-checkbox>
     <el-form-item style="width:100%;">
-      <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录
+      <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit" :loading="logining">登录
       </el-button>
       <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
     </el-form-item>
@@ -22,11 +22,11 @@ export default {
   data () {
     return {
       logining: false,
-      ruleForm2: {
+      ruleForm: {
         account: '张三',
         checkPass: '123'
       },
-      rules2: {
+      rules: {
         account: [{
           required: true, message: '请输入账号', trigger: 'blur'
         }]
@@ -38,11 +38,12 @@ export default {
     }
   },
   methods: {
-    handleSubmit2 () {
-      this.$refs.ruleForm2.validate((valid) => {
+    handleSubmit () {
+      debugger
+      this.$refs.ruleForm.validate((valid) => {
         if (valid) {
           this.logining = true
-          var loginParams = { userName: this.ruleForm2.account, userPassword: this.ruleForm2.checkPass }
+          let loginParams = { userName: this.ruleForm.account, userPassword: this.ruleForm.checkPass }
           userLogin(loginParams).then(result => {
             this.logining = false
             let { code, message, data } = result
