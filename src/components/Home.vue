6285405f80547375d78521ffda3502ba83ea4997
fz <fzyouni@163.com>
2018-07-17 10:32:08 +0800

    主页父组件

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,53 @@
 <template>
-  <p class="page-container">登陆成功</p>
+  <el-container>
+    <v-header :user="user"></v-header>
+    <el-container>
+      <el-aside width="200px">
+        <v-aside></v-aside>
+      </el-aside>
+      <transition>
+        <router-view></router-view>
+      </transition>
+    </el-container>
+  </el-container>
+
 </template>
+<script>
+import header from './header/Header'
+import menu from './menu/Menu'
+
+export default {
+  data () {
+    return {
+      user: JSON.parse(sessionStorage.getItem('user'))
+    }
+  },
+  components: {
+    'v-header': header,
+    'v-aside': menu
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 512px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 470px;
+  }
+</style>
