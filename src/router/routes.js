6fcdd82a4868b4508a3bdc67aed92c9b73b185ca
fz <fzyouni@163.com>
2018-07-17 10:30:27 +0800

    增加404页面

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,17 @@
 import Login from '../components/Login'
 import Home from '../components/Home'
+import NotFound from '../components/Error'
 
 let routes = [
   {
     path: '/login',
     name: '',
     component: Login
+  },
+  {
+    path: '/error',
+    name: '',
+    component: NotFound
   },
   {
     path: '/home',
