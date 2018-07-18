570437301b1ec17d0b0dd987dce0331b716608c6
fz <fzyouni@163.com>
2018-07-18 17:12:40 +0800

    增加路由

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,9 @@
 import Login from '../components/Login'
 import Home from '../components/Home'
 import NotFound from '../components/Error'
+import UserCenter from '../components/user/UserCenter'
+import RoleManagement from '../components/user/RoleManagement'
+import PermissionManagement from '../components/user/PermissionManagement'
 
 let routes = [
   {
@@ -16,7 +19,24 @@ let routes = [
   {
     path: '/home',
     name: '',
-    component: Home
+    component: Home,
+    children: [
+      {
+        path: '/home/userCenter',
+        name: '',
+        component: UserCenter
+      },
+      {
+        path: '/home/roleManage',
+        name: '',
+        component: RoleManagement
+      },
+      {
+        path: '/home/permissionManage',
+        name: '',
+        component: PermissionManagement
+      }
+    ]
   }
 ]
 
